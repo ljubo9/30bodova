187309e9881a3d3fc061dc6fbbebfdb94d57d1b8
perkovicmarija <148704916+perkovicmarija@users.noreply.github.com>
2023-11-11 16:36:52 +0100

    za karla

--- IzvorniKod/kuhaj-it-app/src/App.js
+++ IzvorniKod/kuhaj-it-app/src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from 'react';
 import Navigation from './components/Navigation';
+import Register from './components/Register';
 
 function App() {
+  const [showRegister, setShowRegister] = useState(false);
+
+  const handleRegisterButtonClick = () => {
+    setShowRegister(true);
+  };
+
   return (
     <div className="App">
-      <Navigation />
+      <Navigation onRegisterButtonClick={handleRegisterButtonClick} />
+      {showRegister && <Register />}
     </div>
   );
 }
