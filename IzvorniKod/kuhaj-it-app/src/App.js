3de293ee979f6f8f6959834a1d100bde5365792b
ljubo9 <david.ljubic@fer.hr>
2023-11-11 18:41:20 +0100

    navbar working, routing working

--- IzvorniKod/kuhaj-it-app/src/App.js
+++ IzvorniKod/kuhaj-it-app/src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Routes> 
+          <Route exact path="/login" element={<Login />}/>
+          {/* Add other routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
