6bacd82bc450a0d2faaaf610f13d6fc7331bdbe3
ljubo9 <david.ljubic@fer.hr>
2023-11-11 18:54:30 +0100

    routing done

--- IzvorniKod/kuhaj-it-app/src/App.js
+++ IzvorniKod/kuhaj-it-app/src/App.js
@@ -1,40 +1,21 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import React, { useState } from 'react';
-
 import Navigation from './components/Navigation';
-
+import Login from './components/Login';
 import Register from './components/Register';
 
-import Login from './components/Login';
-
-
 function App() {
-  const [showRegister, setShowRegister] = useState(false);
-
-  const handleRegisterButtonClick = () => {
-    setShowRegister(true);
-  };
-
   return (
     <Router>
       <div className="App">
         <Navigation />
         <Routes> 
           <Route exact path="/login" element={<Login />}/>
+          <Route exact path="/register" element={<Register />}/>
           {/* Add other routes as needed */}
         </Routes>
       </div>
     </Router>
-
-    <div className="App">
-      <Navigation onRegisterButtonClick={handleRegisterButtonClick} />
-      {showRegister && <Register />}
-      <Navigation />
-      <Login />
-    </div>
   );
 }
 
