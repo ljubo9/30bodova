df63136b45a6a8ab9cc88d210b9d72e3b350f470
Hana <hana.ivancic@fer.hr>
2024-01-13 17:06:01 +0100

    added elements of LoggedHomePage

--- IzvorniKod/kuhaj-it-app/src/components/LoggedHomePage.jsx
+++ IzvorniKod/kuhaj-it-app/src/components/LoggedHomePage.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Card, Container, Col, Row } from 'react-bootstrap';
-import cookingImage from '../assets/cooking.png'; 
 
 function LoggedHomePage() {
   const { username } = useParams();
@@ -12,15 +10,17 @@ function LoggedHomePage() {
     const fetchRecipeList = async () => {
       try {
         //ruta za recepte koje je neki user konzumiro
-        const response = await fetch('https://kuhajitbackend.onrender.com/recepies/user/${username}');
+        //svakom useru trebalo bi dodati listu recepata koje je konzumirao
+        //al ja bi to dodala ko mapu di je ključ taj recept a vrijednost je lista datuma kad je sve taj recept konzumirao jer nam to treba za statistiku
+        const response = await fetch('https://kuhajitbackend.onrender.com/recipes/user/${username}');
         if (response.ok) {
           const data = await response.json();
           setRecipeList(data);
         } else {
-          console.error('Error fetching profile data:', response.statusText);
+          console.error('Error fetching recipes:', response.statusText);
         }
       } catch (error) {
-        console.error('Error fetching profile data:', error.message);
+        console.error('Error fetching recipes:', error.message);
       }
     };
 
@@ -30,14 +30,91 @@ function LoggedHomePage() {
   if (!recipeList) {
     return <div>Loading...</div>;
   }
+
+  const [dietInfo, setDiet] = useState(null);
+
+  useEffect(() => {
+    const fetchDietInfo = async () => {
+      try {
+        //ruta za dijetu koja je dodijeljena useru - trebamo napravit entitet za dijetu, user ima jednu dijetu
+        //dijeta ima listu recepata koje smije konzumirati - msm da je tak lakse
+        const response = await fetch('https://kuhajitbackend.onrender.com/diet/user/${username}');
+        if (response.ok) {
+          const data = await response.json();
+          setDiet(data);
+        } else {
+          console.error('Error fetching diet:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching diet:', error.message);
+      }
+    };
+
+    fetchDietInfo();
+  }, [username]);
+
+  if (!dietInfo) {
+    return <div>Loading...</div>;
+  }
+
+  const [followedChefs, setFollowedChefs] = useState(null);
+
+  useEffect(() => {
+    const fetchFollowedChefs = async () => {
+      try {
+        //ruta za listu entuzijasta koje nas user prati, msm da to samo mozemo dodat listu ko atribut usera
+        const response = await fetch('https://kuhajitbackend.onrender.com/followed/user/${username}');
+        if (response.ok) {
+          const data = await response.json();
+          setFollowedChefs(data);
+        } else {
+          console.error('Error fetching diet:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching diet:', error.message);
+      }
+    };
+
+    fetchFollowedChefs();
+  }, [username]);
+
+  if (!followedChefs) {
+    return <div>Loading...</div>;
+  }
+
+  const [consumedRecipesStatistics, setConsumedRecipesStatistics] = useState(null);
+
+  useEffect(() => {
+    const fetchConsumedRecipesStatistics = async () => {
+      try {
+        //ruta za statistiku usera - pogledat u zadatku kaj tocno
+        const response = await fetch('https://kuhajitbackend.onrender.com/statistic/user/${username}');
+        if (response.ok) {
+          const data = await response.json();
+          setConsumedRecipesStatistics(data);
+        } else {
+          console.error('Error fetching diet:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching diet:', error.message);
+      }
+    };
+
+    fetchConsumedRecipesStatistics();
+  }, [username]);
+
+  if (!consumedRecipesStatistics) {
+    return <div>Loading...</div>;
+  }
   
   return (
     <Container>
-      <Row>
-        {recipeList.map(recipe => (
+      <Row className="mt-4">
+        <Col>
+          <h2>Isprobani recepti</h2>
+          {recipeList.map(recipe => (
           <Col key={recipe.id} md={4}>
             <Card className="mb-4">
-              <Card.Img variant="top" src={cookingImage} />
               <Card.Body>
                 <Card.Title>{recipe.title}</Card.Title>
                 <Card.Text>
@@ -60,6 +137,36 @@ function LoggedHomePage() {
             </Card>
           </Col>
         ))}
+        </Col>
+      </Row>
+
+      <Row className="mt-4">
+        <Col>
+          <h2>Dijeta</h2>
+          <p>{dietInfo}</p>
+        </Col>
+      </Row>
+
+      <Row className="mt-4">
+        <Col>
+          <h2>Nove kuharice</h2>
+          {followedChefs.map((chef, index) => (
+            <Card key={index} className="mb-3">
+              <Card.Body>
+                <Card.Title>{chef.name}</Card.Title>
+                {/* Dodajte ostale informacije o kuharici koje želite prikazati */}
+              </Card.Body>
+            </Card>
+          ))}
+        </Col>
+      </Row>
+
+      <Row className="mt-4">
+        <Col>
+          <h2>Statistika konzumiranih nutritivnih vrijednosti</h2>
+          {/* Dodajte prikaz statistike konzumiranih nutritivnih vrijednosti */}
+          <p>{consumedRecipesStatistics}</p>
+        </Col>
       </Row>
     </Container>
   );
