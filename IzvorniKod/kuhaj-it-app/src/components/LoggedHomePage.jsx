ad7dfb72f8669c783193f2053746ebc1f7f9547a
Hana <hana.ivancic@fer.hr>
2024-01-13 16:28:10 +0100

    updated return statement for LoggedHomePage

--- IzvorniKod/kuhaj-it-app/src/components/LoggedHomePage.jsx
+++ IzvorniKod/kuhaj-it-app/src/components/LoggedHomePage.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Card, Image } from 'react-bootstrap';
+import { Card, Container, Col, Row } from 'react-bootstrap';
 import cookingImage from '../assets/cooking.png'; 
 
-function HomePage() {
+function LoggedHomePage() {
   const { username } = useParams();
   const [recipeList, setRecipeList] = useState(null);
 
@@ -12,7 +12,7 @@ function HomePage() {
     const fetchRecipeList = async () => {
       try {
         //ruta za recepte koje je neki user konzumiro
-        const response = await fetch('http://localhost:8080/recepies/user/${username}');
+        const response = await fetch('https://kuhajitbackend.onrender.com/recepies/user/${username}');
         if (response.ok) {
           const data = await response.json();
           setRecipeList(data);
@@ -32,28 +32,22 @@ function HomePage() {
   }
   
   return (
-    <>
-      <div
-        style={{
-          height: '100vh',
-          backgroundImage: `url(${cookingImage})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center', 
-        }}
-      >
-        <div className="d-flex justify-content-center align-items-center position-relative" style={{ height: '100vh' }}>
-          <Card className="bg-warning bg-gradient p-5">
-            <Card.Title>
-              <h1>Dobrodošli u KuhajIT!</h1>
-            </Card.Title>
-            <Card.Text>
-              <h5>Istražite kreativne recepte, dijete i započnite novo kulinarsko iskustvo!</h5>
-            </Card.Text>
-          </Card>
-        </div>
-      </div>
-    </>
+    <Container>
+      <Row>
+        {recipeList.map(recipe => (
+          <Col key={recipe.id} md={4}>
+            <Card className="mb-4">
+              <Card.Img variant="top" src={cookingImage} />
+              <Card.Body>
+                <Card.Title>{recipe.title}</Card.Title>
+                {/* Dodajte ostale informacije o receptu koje želite prikazati */}
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
-export default HomePage;
+export default LoggedHomePage;
