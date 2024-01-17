17c98d15c6b6189ca1d7efa224a7abab103f7d1c
Ivan Pavlovic <ivan.pavlovic200@gmail.com>
2024-01-17 17:15:36 +0100

    minor

--- IzvorniKod/src/main/java/app/recipe/Recipe.java
+++ IzvorniKod/src/main/java/app/recipe/Recipe.java
@@ -33,7 +33,15 @@ public class Recipe {
     private List<RecipeIngredient> ingredients;
 
 
-    
+    public Recipe( int id,String name, List<RecipeIngredient> ingredients,List<StepOfMaking> stepsOfMaking, int portionSize, int cookTime) {
+        this.id=id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.stepsOfMaking = stepsOfMaking;
+        this.portionSize = portionSize;
+        this.cookTime = cookTime;
+
+    }
     public Recipe( String name, List<RecipeIngredient> ingredients,List<StepOfMaking> stepsOfMaking, int portionSize, int cookTime) {
 
         this.name = name;
