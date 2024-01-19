1034a35c899c879c7d665bc956ea65884985d38c
ljubo9 <david.ljubic@fer.hr>
2024-01-19 02:29:25 +0100

    sori karlo nisam mogao gledat odvratan inline css, pa sam zamjenio sve s bootstrapom :)

--- IzvorniKod/kuhaj-it-app/src/components/ChooseRecipe.jsx
+++ IzvorniKod/kuhaj-it-app/src/components/ChooseRecipe.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
 import { Link } from 'react-router-dom';
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 
 
@@ -157,79 +158,87 @@ const ChooseRecipe = () => {
 
 
   return (
-    // className "flex flex-col"
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div className="bg-secondary p-2 min-vh-100"> 
+    <Container className="mt-4 bg-light">
       {/* First Row */}
-      <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '20px' }}>
+      <Row className="mb-4 mt-3 p-3">
         {/* Left Column: QR Scanner */}
-        <div style={{ flex: 1, display: 'flex', flexDirection: 'column' , justifyContent: 'center', alignItems: 'center'}}>
-          <h3>Skeniraj namirnicu:</h3>
-          <div id="reader" style={{ width: 'fit-content' }} />
-        </div>
+        <Col md={6}>
+          <div className="bg-white p-3 rounded text-center">
+            <h3>Skeniraj namirnicu:</h3>
+            <div id="reader" className="w-content" />
+          </div>
+        </Col>
 
         {/* Right Column: Product Selection */}
-        <div style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          {/* Product Selection and List */}
-          <div>
-            <h3>Odaberi namirnicu:</h3>
-            {/* Select Product */}
-            <select
-              value={selectedProduct}
-              onChange={(e) => setSelectedProduct(e.target.value)}
-            >
-              <option value="">Odaberi namirnicu</option>
-                {ingredients.map((ingredient, index) => (
-                 <option key={index} value={ingredient.name}>
-                    {ingredient.name}
-                  </option>
-                ))}
-            </select>
-            <button onClick={() => addProduct(selectedProduct)}>Dodaj</button>
+        <Col md={6}>
+          <div className="bg-white p-3 rounded text-center">
+            {/* Product Selection and List */}
+            <div>
+              <h3>Odaberi namirnicu:</h3>
+              {/* Select Product */}
+              <Form.Group>
+                <Form.Control
+                  as="select"
+                  value={selectedProduct}
+                  onChange={(e) => setSelectedProduct(e.target.value)}
+                >
+                  <option value="">Odaberi namirnicu</option>
+                  {ingredients.map((ingredient, index) => (
+                    <option key={index} value={ingredient.name}>
+                      {ingredient.name}
+                    </option>
+                  ))}
+                </Form.Control>
+              </Form.Group>
+              <Button onClick={() => addProduct(selectedProduct)} className="mt-3" variant="dark">Dodaj</Button>
+            </div>
 
+            {/* List of Products */}
+            <div>
+              {Object.entries(products).map(([product, count], index) => (
+                <div key={index} className="d-flex align-items-center justify-content-start m-2">
+                  {/* Column for Product Name */}
+                  <span className="mr-3" style={{ width: '150px' }}>{product}:</span>
 
-          </div>
-
-          {/* List of Products */}
-          <div>
-            {Object.entries(products).map(([product, count], index) => (
-              <div key={index} style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', margin: '10px' }}>
-                {/* Column for Product Name */}
-                <span style={{ marginRight: '20px', width: '150px' }}>{product}:</span>
-
-                {/* Row for +, Count and - Buttons */}
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <button onClick={() => decrementProductCount(product)} style={{ marginRight: '5px', border: 'none', outline: 'none', backgroundColor: 'transparent' }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
-                    </svg>
-                  </button>
-                  <span style={{ marginRight: '5px' }}>{count}</span>
-                  <button onClick={() => incrementProductCount(product)} style={{ border: 'none', outline: 'none', backgroundColor: 'transparent' }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
-                    </svg>
-                  </button>
+                  {/* Row for +, Count and - Buttons */}
+                  <div className="d-flex align-items-center">
+                    <Button onClick={() => decrementProductCount(product)} className="mr-2" variant="link">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z" />
+                      </svg>
+                    </Button>
+                    <span className="mr-2">{count}</span>
+                    <Button onClick={() => incrementProductCount(product)} variant="link">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                      </svg>
+                    </Button>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
 
-        </div>
-      </div>
+          </div>
+        </Col>
+      </Row>
 
       {/* Second Row: Recipe Cards */}
-      <div>
+      <Row>
         {recepti && typeof recepti !== undefined && recepti.length > 0 && recepti.map((recipe, index) => (
-          <div key={index} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', width: '300px' }}>
-            <Link to={`/recipe/${recipe.id}`}>
-              <h3>{recipe.name}</h3>
-            </Link>
-            <p>Podudarni sastojci: {recipe.matchCount}/{recipe.ingredients?.length}</p>
-          </div>
+          <Col md={4} key={index}>
+            <div className="bg-white p-3 rounded mb-4">
+              <Link to={`/recipe/${recipe.id}`} className="text-decoration-none text-dark">
+                <h3>{recipe.name}</h3>
+              </Link>
+              <p>Podudarni sastojci: {recipe.matchCount}/{recipe.ingredients?.length}</p>
+            </div>
+          </Col>
         ))}
-      </div>
+      </Row>
+    </Container>
     </div>
-  );  
+  );
 };
 
 export default ChooseRecipe;
