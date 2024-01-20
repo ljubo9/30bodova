6a2930d9703ec7f9d4ac055db87940a0fbc24597
Fran <fran.siric@gmail.com>
2024-01-20 01:22:23 +0100

    fixes for stupid mistakes

--- IzvorniKod/kuhaj-it-app/src/components/CookbookEditor.jsx
+++ IzvorniKod/kuhaj-it-app/src/components/CookbookEditor.jsx
@@ -17,7 +17,7 @@ const CookbookEditor = () => {
 
     try {
       {/* slanje cookbook.name i cookbook.category i creatora na endpoint */}
-      const response = await fetch('https://kuhajitbackend.onrender.com/cookbooks', {
+      const response = await fetch('https://kuhajitbackend.onrender.com/cookbook', {
         method: 'POST',
         body: formData,
       });
